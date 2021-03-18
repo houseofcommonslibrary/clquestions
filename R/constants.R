aef62a2895cafc6f77a0cb38925093b5899c3223
dempseynoel <noel.dempsey@outlook.com>
2021-03-18 10:26:49 +0000

    0.4.1
    Updated Namespace

--- R/constants.R
+++ R/constants.R
@@ -1,6 +1,10 @@
 ### Package constants
 
+# Questions
 WQ_BASE_URL <- "https://writtenquestions-api.parliament.uk/api/writtenquestions/questions?expandMember=true"
+OQ_BASE_URL <- ""
+
+# Statements and EDMs
 WS_BASE_URL <- "https://writtenquestions-api.parliament.uk/api/writtenstatements/statements?expandMember=true"
 
 library(magrittr)
